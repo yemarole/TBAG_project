e9684622401df8d2c68cc4e76a92f748e92f51ac
yemarole <yemiarole@icloud.com>
2025-01-17 10:55:28 +0000

    linking rooms

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from room import Room
 
 kitchen = Room("kitchen")
-ballroom = Room("ballroom")
-dining_hall = Room("dining hall")
-
 kitchen.set_description("A dank and dirty room buzzing with flies.")
+ballroom = Room("ballroom")
+ballroom.set_description("A vast room with shiny wooden floors.")
+dining_hall = Room("dining hall")
+dining_hall.set_description("A large room with ornate golden decorations on each wall.")
 
-print(kitchen.describe())
+# print(kitchen.describe())
+
+kitchen.link_room(dining_hall, "south")
+dining_hall.link_room(kitchen, "north")
+dining_hall.link_room(ballroom, "west")
+ballroom.link_room(dining_hall, "east")
+
+dining_hall.get_details()
--- room.py
+++ room.py
@@ -2,6 +2,7 @@ class Room:
     def __init__(self, room_name):
         self.name = room_name
         self.description = None
+        self.linked_rooms = {}
 
     def get_description(self):
         return self.description
@@ -17,3 +18,14 @@ class Room:
 
     def get_name(self):
         return self.name
+
+    def link_room(self, room_to_link, direction):
+        self.linked_rooms[direction] = room_to_link
+
+    def get_details(self):
+        print(self.name)
+        print("--------------------")
+        print(self.description)
+        for direction in self.linked_rooms:
+            room = self.linked_rooms[direction]
+            print(f"The {room.get_name()} is {direction}.")
